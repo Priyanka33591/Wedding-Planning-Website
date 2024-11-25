c63234d2a9a9968a12d5e59f5ed19865c48b74e4
Priyanka33591 <priyanka33591@gmail.com>
2024-11-26 00:24:21 +0530

    update team member photo

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -7,17 +7,17 @@ const teamMembers = [
     {
       name: 'Hritik Chouhan',
       role: 'Wedding Planner',
-      image: 'https://randomuser.me/api/portraits/men/1.jpg'  
+      image: 'https://res.cloudinary.com/ddbcdcws5/image/upload/v1732560715/WhatsApp_Image_2024-11-26_at_00.20.20_730e9453_tjzzva.jpg'  
     },
     {
       name: 'Priyanka Yadav',
       role: 'Wedding Planner',
-      image: 'https://randomuser.me/api/portraits/women/1.jpg' 
+      image: 'https://media.licdn.com/dms/image/v2/D5603AQEoqBMr8QVIaw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712638033700?e=1738195200&v=beta&t=rNpt5jYwU9QcfVbblEgKbFACNYC5tTwhonSUKm7MjQQ' 
     },
     {
       name: 'Abhishek Singh',
       role: 'Wedding Planner',
-      image: 'https://randomuser.me/api/portraits/men/2.jpg'
+      image: 'https://media.licdn.com/dms/image/v2/D5603AQGNYFdoqcUtKw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1723719184048?e=1738195200&v=beta&t=vvaH_fNz4eT5Vc43gRBKII1xA760Lhi6mZD_Lb067b4'
     },
     {
       name: 'Shreyanshi Jain',
